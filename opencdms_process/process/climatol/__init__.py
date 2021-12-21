cb71ad8a16399342c4b892e20d59c6c3b5b12799
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-22 02:16:54 +0600

    escape curly braces

--- opencdms_process/process/climatol/__init__.py
+++ opencdms_process/process/climatol/__init__.py
@@ -31,16 +31,15 @@ def windrose(obs):
 
     r(f"""
     library('magick')
-    windrose <- function(data, code='', name='', uni='m/s', maxnsc=8, fnum=4,
-      fint=5, flab=2, col=rainbow(10,.5,.92,start=.33,end=.2), ang=3*pi/16,
-      margin=c(0,0,4,0)) {
+
+    windrose <- function(data, code='', name='', uni='m/s', maxnsc=8, fnum=4, fint=5, flab=2, col=rainbow(10,.5,.92,start=.33,end=.2), ang=3*pi/16, margin=c(0,0,4,0)) {{
       z <- range(data[,1])
       startdate <- as.Date(z[1]); enddate <- as.Date(z[2])
       dd <- data[,2]; vv <- data[,3]; rm(data)
       dd[dd<0 | dd>360] <- NA
       vv[is.na(dd)] <- NA
       nd <- sum(!is.na(vv))
-      if(nd==0) stop('No data available for that station and dates\n')
+      if(nd==0) stop('No data available for that station and dates')
       cal <- sum(vv==0,na.rm=TRUE)
       dd <- round(dd/360*16+1); dd[dd==17] <- 1
       vm <- tapply(vv,as.factor(dd),mean,na.rm=TRUE)
@@ -52,30 +51,30 @@ def windrose(obs):
       vvc <- pretty(vv)
       nvvc <- length(vvc)-1
 
-      if(nvvc>maxnsc) {
+      if(nvvc>maxnsc) {{
         nvvc <- maxnsc
         vvf <- cut(vv,c(vvc[1:maxnsc],999))
         spclasses <- paste(vvc[1:nvvc],vvc[2:(nvvc+1)],sep='-')
         if(latex) spclasses[nvvc] <- paste('>=',vvc[maxnsc])
         else spclasses[nvvc] <- paste('>=',vvc[maxnsc])
         geflag <- TRUE
-      } else {
+      }} else {{
         vvf <- cut(vv,vvc)
         spclasses <- paste(vvc[1:nvvc],vvc[2:(nvvc+1)],sep='-')
         geflag <- FALSE
-      }
+      }}
 
       fr <- table(vvf,dd)
 
       z <- as.integer(colnames(fr))
-      if(length(z)<16) {
+      if(length(z)<16) {{
         zrn <- rownames(fr)
         zm <- matrix(rep(0,16*nrow(fr)),dim(fr))
         zm[,z] <- fr
         fr <- zm
         colnames(fr) <- as.character(1:16)
         rownames(fr) <- zrn
-      }
+      }}
 
       nd1 <- sum(fr[1,]); fr[1,] <- fr[1,] * (nd1+cal)/nd1
       if(nd>0) fr <- fr*100./nd
@@ -102,20 +101,20 @@ def windrose(obs):
       fy <- sin(pi/2-(2*pi/ndir*0:(ndir-1)))
       plot(fx,fy,xlim=c(-fmax-mlf*fint,fmax+fint),ylim=c(-fmax-fint,fmax+fint),
         xaxt="n",yaxt="n",xlab="",ylab="",bty="n",asp=1,type="n")
-      if(nr==1) {
+      if(nr==1) {{
         cx <- fx*fr
         cy <- fy*fr
-      }
-      else {
+      }}
+      else {{
         f <- apply(fr,2,sum)
         cx <- fx*f
         cy <- fy*f
-        for(i in nr:2) {
+        for(i in nr:2) {{
           f <- f-fr[i,]
           cx <- c(cx,NA,fx*f)
           cy <- c(cy,NA,fy*f)
-        }
-      }
+        }}
+      }}
       polygon(cx,cy,col=col[nr:1])
       symbols(c(0*1:fnum),c(0*1:fnum),circles=c(fint*1:fnum),inches=FALSE,add=TRUE)
       segments(0*1:ndir,0*1:ndir,fmax*fx,fmax*fy)
@@ -128,14 +127,13 @@ def windrose(obs):
         for(i in 1:fnum) text(i*fint*cos(ang),i*fint*sin(ang),paste(i*fint,"%"))
       else if(flab==1)
         text(fmax*cos(ang),fmax*sin(ang),paste(fmax,"%"))
-      if(key) {
+      if(key) {{
         legend(-fmaxi-2.3*fint,fmaxi+2,fill=col,legend=spclasses)
         text(-fmaxi-1.5*fint,fmaxi+.9*fint,uni)
-      }
-      title(paste(name,'windrose\n',startdate,'to',enddate))
+      }}
+      title(paste(name,'windrose',startdate,'to',enddate))
       invisible(tab)
-    }
-
+    }}
     figure <- image_graph(width = 350, height = 350, res = 96)
 
     data=observations
